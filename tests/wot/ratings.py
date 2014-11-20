01ca2a9de6ca167aedd65c0fda2cef466bd92559
svartalf <self@svartalf.info>
2014-11-20 17:48:20 +0300

    Some WoT ratings tests fixes

--- tests/wot/ratings.py
+++ tests/wot/ratings.py
@@ -7,7 +7,7 @@ class WOTRatingsTestCase(WargamingTestCase):
 
     def setUp(self):
         super(WOTRatingsTestCase, self).setUp()
-        self.account_id = 1000000000
+        self.account_id = 1000000001
         self.rank_field = 'frags_count'
 
     def test_types(self):
@@ -17,7 +17,8 @@ class WOTRatingsTestCase(WargamingTestCase):
         self.assertValidResponse(self.api.ratings.dates, type='all')
 
     def test_accounts(self):
-        self.assertValidResponse(self.api.ratings.accounts, type='all')
+        self.assertValidResponse(self.api.ratings.accounts, type='all',
+                                 account_id=self.account_id)
 
     def test_neighbors(self):
         self.assertValidResponse(self.api.ratings.neighbors, type='all',
